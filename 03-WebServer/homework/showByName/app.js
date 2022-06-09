193e191d63814d4691b73915a0a154e697f2a263
clanrosso <claudiorossocba@hotmail.com.ar>
2022-06-08 21:51:16 -0300

    ready

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,17 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
 
 // Escribí acá tu servidor
+http
+  .createServer(function (req, res) {
+    fs.readFile(__dirname + "/images" + req.url + ".jpg", (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Hubo un error");
+      } else {
+        res.writeHead(200, { "Content-Type": "image/jpg" });
+        res.end(data);
+      }
+    });
+  })
+  .listen(1337, "127.0.0.1");
